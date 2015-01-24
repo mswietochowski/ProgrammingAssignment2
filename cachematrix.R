901305905314b5c2d557395bfda30c8bedc1235b
Maciej Andrzej Świętochowski <m@swietochowski.eu>
2015-01-24 23:39:33 +0100

    Functions implemented.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,38 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    cachedInverse <- NULL
+    set <- function(y)
+    {
+        x <<- y
+        cachedInverse <<- NULL
+    }
+    get <- function() x
+    getInverse <- function() cachedInverse
+    setInverse <- function(inverse)
+    {
+        cachedInverse <<- y
+    }
+    list(set = set,
+         get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getInverse()
+    if (is.null(inverse))
+    {
+        message("calculating inverse")
+        inverse <- solve(x$get(), ...)
+        x$setInverse(inverse)
+    }
+    else
+    {
+        message("returning cached value")
+    }
+    inverse
 }
